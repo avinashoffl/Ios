f9e5b858a9f3eb3e0edda0c56d6f2a9f23f99afd
avinashoffl <avinashgithun@gmail.com>
2024-09-06 17:10:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7235368503:AAF5ld7gbMj6mG_WhqUBR0R45Nw-")
+BOT_TOKEN=("7333160558:AAHP70T_iJ0AV5Sfx_0rGz7xNFYbAryWEB8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("username")
+OWNER_USERNAME=("IOSOWNER")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("620779474")
+ADMIN_IDS=("1499994203","6682104026")
 
 
 
